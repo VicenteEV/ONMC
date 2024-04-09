c293906450719796db2848433c2ad47d14583113
Haohui <clmhaohui@gmail.com>
2024-04-09 13:51:16 +0200

    Conexion con la bbdd en registro(falta poner fecha))

--- src/onmc/RegistroController.java
+++ src/onmc/RegistroController.java
@@ -7,14 +7,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import utilidades.bbdd.Bd;
+import static utilidades.bbdd.Bd.crearBBDD;
+import utilidades.bbdd.Gestor_conexion_POSTGRE;
+
 
 
 public class RegistroController {
     
     @FXML
-    private Button config, rAceptar;
+    Button config, rAceptar;
+    @FXML
+    TextField rUsuario, rCorreo, rNombre, rApellidos;
+    @FXML
+    DatePicker rNacimiento;
+    @FXML
+    PasswordField rContrasenya;
+    
+    Gestor_conexion_POSTGRE conection = new Gestor_conexion_POSTGRE("juego", true);
+    
     
     @FXML
     public void btnConfig(ActionEvent event) {
@@ -31,14 +46,31 @@ public class RegistroController {
     
     @FXML
     public void btnRAceptar(ActionEvent event) {
+        
+        String consulta;
+        String user = "'" + rUsuario.getText()+"'";
+        String password = "'" + rContrasenya.getText()+"'";
+        String mail = "'" + rCorreo.getText()+ "'";
+        String name =  "'" + rNombre.getText()+ "'";
+        String secondName =  "'" + rApellidos.getText()+ "'";
+        
+        
+        consulta = "insert into usuario (usuario, contrasenya, correo_electronico, nombre, apellidos) values ("+ user + "," + password + "," + mail + "," + name + "," + secondName + ");";
+        
+        Bd.consultaModificacion(conection, consulta);
+        
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Escenas/Inicio.fxml"));
             Stage stage = (Stage) rAceptar.getScene().getWindow();
             Scene scene = new Scene(loader.load());
             stage.setScene(scene);
             stage.setFullScreen(true);
+            
         }catch (IOException io){
             io.printStackTrace();
         }
-    }   
+        
+    }
+    
+
 }
