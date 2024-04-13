23023666b92534401bccb77457f7055f17c50f57
Haohuii <clmhaohui@gmail.com>
2024-04-13 19:08:51 +0200

    En RegistreController se hash la contrasenya

--- src/onmc/RegistroController.java
+++ src/onmc/RegistroController.java
@@ -14,6 +14,9 @@ import javafx.stage.Stage;
 import utilidades.bbdd.Bd;
 import static utilidades.bbdd.Bd.crearBBDD;
 import utilidades.bbdd.Gestor_conexion_POSTGRE;
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 
 
 
@@ -45,7 +48,7 @@ public class RegistroController {
     }
     
     @FXML
-    public void btnRAceptar(ActionEvent event) {
+    public void btnRAceptar(ActionEvent event) throws Exception {
         
         String consulta;
         String user = "'" + rUsuario.getText()+"'";
@@ -55,8 +58,12 @@ public class RegistroController {
         String secondName =  "'" + rApellidos.getText() + "'";
         String bData = "'" + rNacimiento.getValue() + "'";
         
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] digest = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        String sha256 = "'" + DatatypeConverter.printHexBinary(digest).toLowerCase() + "'";
         
-        consulta = "insert into usuario (usuario, contrasenya, correo_electronico, nombre, apellidos, fecha_nacimiento) values ("+ user + "," + password + "," + mail + "," + name + "," + secondName + "," + bData + ");";
+        
+        consulta = "insert into usuario (usuario, contrasenya, correo_electronico, nombre, apellidos, fecha_nacimiento) values ("+ user + "," + sha256 + "," + mail + "," + name + "," + secondName + "," + bData + ");";
         
         Bd.consultaModificacion(conection, consulta);
         
